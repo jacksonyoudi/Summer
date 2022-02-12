9ea05cbf280ce063b90b797243311e27ad71038a
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-02-12 22:48:58 +0800

    rain water

---
+++ RainWater/src/test/java/com/youdi/water/MyTest.java
@@ -0,0 +1,18 @@
+package com.youdi.water;
+
+import com.youdi.pojo.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    @Test
+    public void testHello() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Student student = (Student) context.getBean("student");
+
+        System.out.println(student.getName());
+
+    }
+}
