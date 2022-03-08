c489f3374e57b636ee139c8e66c71f29c940feee
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-03-08 21:49:19 +0800

    annotations

--- Mariadb/src/main/java/com/youdi/mapper/UserMapper.java
+++ Mariadb/src/main/java/com/youdi/mapper/UserMapper.java
@@ -1,9 +1,20 @@
 package com.youdi.mapper;
 
 import com.youdi.pojo.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface UserMapper {
-    public List<User> selectUser();
+
+    @Select("select * from user")
+    List<User> selectUser();
+
+    @Select("select * from user where id=#{id}")
+    User getUserById(@Param("id") int id);
+
+    @Insert("insert into user (id, name, password) value (#{id}, #{name}, #{password}})")
+    int addUser(User user);
 }
