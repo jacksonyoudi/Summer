866e39834a10fca3f32b74267234b180604894b4
youdi <liangchangyoujackson@gmail.com>
2022-03-26 16:38:02 +0800

    mybatis

--- Mariadb/src/main/java/com/youdi/mariadb/MariadbApplication.java
+++ Mariadb/src/main/java/com/youdi/mariadb/MariadbApplication.java
@@ -1,13 +1,10 @@
 package com.youdi.mariadb;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class MariadbApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MariadbApplication.class, args);
+
     }
 
 }
--- Mariadb/src/test/java/com/youdi/pojo/UserTest.java
+++ Mariadb/src/test/java/com/youdi/pojo/UserTest.java
@@ -1,2 +1,28 @@
 package com.youdi.pojo;
 
+
+import com.youdi.mapper.UserMapper;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.util.List;
+
+
+class UserTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-dao.xml");
+        UserMapper mapper = context.getBean("userMapper", UserMapper.class);
+        List<User> users = mapper.selectUser();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        context.close();
+    }
+
+    @Test
+    public void user() throws IOException {
+
+    }
+}
+
